d3f2b51c6e216d1b4c7f76d768cbc02e71148a22
jackd <jackdrulman@ou.edu>
2020-09-01 11:49:42 -0500

    Created new repository for starter soultions + projects

---
+++ Participation1_SumOfNuumbers/Participation1_SumOfNuumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Participation1_SumOfNuumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
