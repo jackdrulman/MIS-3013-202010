2d7bbd8e6fb8eba966fbad37ba1daa8a293fa694
jackd <jackdrulman@ou.edu>
2020-09-01 12:13:21 -0500

    finished the inputoutput example for class

--- Participation1_SumOfNuumbers/Participation1_SumOfNuumbers/Program.cs
+++ Participation1_SumOfNuumbers/Participation1_SumOfNuumbers/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿// collaborated with class
+
+using System;
 
 namespace Participation1_SumOfNuumbers
 {
     class Program
     {
+
+        const string CLASS_NAME = "MIS 3013";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           // this won't work bc it is a constant CLASS_NAME = "MIS 3353";
+
+            string firstname = "jack";
+            string lastname = "drulman";
+
+            Console.WriteLine("please enter your first name >>");
+            firstname = Console.ReadLine(); //getting what the user typed and stored in the
+
+            Console.WriteLine("please enter your last name >>");
+            lastname = Console.ReadLine();
+
+            Console.WriteLine("what is your GPA? >>");
+            string gpaInput = Console.ReadLine();
+            double gpa = Convert.ToDouble(gpaInput);
+
+            
+            Console.WriteLine(firstname + " " + lastname + " is apart of " + CLASS_NAME + " with a GPA of " +
+                gpa.ToString("n2"));
+
+
+
         }
     }
 }
